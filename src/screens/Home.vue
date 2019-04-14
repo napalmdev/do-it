ccd3f8a530aeabff12b13044dc438b5631060af6
Denis Nunes <denis_tecnosoft@hotmail.com>
2019-04-14 18:52:27 -0300

    Init project with standart codestyle

---
+++ src/screens/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="~@/assets/img/logo.svg">
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+export default {
+  name: 'home',
+}
+</script>
+
+<style lang="scss" scoped>
+  .home {
+    width: 400px;
+    height: 400px;
+    background-color: $primary-color;
+  }
+</style>
